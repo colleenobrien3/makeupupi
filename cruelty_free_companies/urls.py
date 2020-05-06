19aa62b3b92ea1fa099f32cf19599a5a2316ed5a
Colleen_O'Brien <colleen3117@gmail.com>
2020-05-06 14:51:32 -0400

    added redirect

--- cruelty_free_companies/urls.py
+++ cruelty_free_companies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CompanyList
+from .views import CompanyList, redirect_view
 
 urlpatterns = [
-    path('companies', CompanyList.as_view(), name='company_list')
+    path('companies', CompanyList.as_view(), name='company_list'),
+    path('', redirect_view)
 ]
--- cruelty_free_companies/views.py
+++ cruelty_free_companies/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import CompanySerializer
 from .models import Company
+from django.shortcuts import redirect
 
 
 class CompanyList(generics.ListAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+
+def redirect_view(request):
+    response = redirect('/companies')
+    return response
